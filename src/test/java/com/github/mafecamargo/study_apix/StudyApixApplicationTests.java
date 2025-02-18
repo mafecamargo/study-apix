753f8e080d80f83d3c33d5c9b5686d01133be3ec
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-17 22:45:04 -0300

    first commit

---
+++ src/test/java/com/github/mafecamargo/study_apix/StudyApixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mafecamargo.study_apix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
